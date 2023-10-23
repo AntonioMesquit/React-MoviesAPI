d1dbd828adc70bcd6d73297706a78e72fe2f31aa
antonio <antonio109mesquita@gmail.com>
2023-10-23 06:18:26 -0300

    yu

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require("express-async-errors")
+//instalar o cors ,para o front end conseguir fazer requisicoes para o back, npm install cors
+const cors = require('cors')
 const AppError = require("./utils/AppError")
 const express = require("express")
 const routes = require("./routes")
@@ -6,9 +8,12 @@ const uploadConfig = require("./configs/upload")
 //instalamos o knex, para ser o nosso query builder usando npm install kex
 //para abrimos a configuracao dele usamos npx knex init
 
+//para trabalhar com requisicoes http, instalar o npm install axios no front end
 const migrationsRun = require("./database/sqlite/migrations")
 const app = express();
+app.use(cors())
 app.use(express.json())
+
 app.use("/files" , express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 migrationsRun() 
